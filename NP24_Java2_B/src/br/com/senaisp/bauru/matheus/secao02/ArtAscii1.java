69fb7c02ada62fe52d684d5b48035e0bde0fc424
MatthewsR1 <matheus10snunes@gmail.com>
2024-03-13 22:05:50 -0300

    conteudo da aula do dia 13-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/matheus/secao02/ArtAscii1.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.matheus.secao02;
+
+public class ArtAscii1 {
+
+	public static void main(String[] args) {
+      System.out.println("   ████  ");
+      System.out.println("  █    █ ");
+      System.out.println(" █ █  █ █ ");
+      System.out.println(" █      █ ");
+      System.out.println(" █ █  █ █  ");
+      System.out.println(" █  ██  █ " );
+      System.out.println("  █    █  ");
+      System.out.println("   ████  ");
+	}
+
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/matheus/secao02/Arteascci2.java
@@ -0,0 +1,15 @@
+package br.com.senaisp.bauru.matheus.secao02;
+
+public class Arteascci2 {
+
+	public static void main(String[] args) {
+          System.out.println("__________________|      |____________________________________________\r\n"
+          		+ "     ,--.    ,--.          ,--.   ,--.\r\n"
+          		+ "    |oo  | _  \\  `.       | oo | |  oo|\r\n"
+          		+ "o  o|~~  |(_) /   ;       | ~~ | |  ~~|o  o  o  o  o  o  o  o  o  o  o\r\n"
+          		+ "    |/\\/\\|   '._,'        |/\\/\\| |/\\/\\|\r\n"
+          		+ "__________________        ____________________________________________\r\n"
+          		+ "                  |      |dwb");
+	}
+
+}
